105fad289560cf9e4731dc02922b1e4523eb91e7
Mario Vaquero <mariovquero@gmail.com>
2025-09-05 20:44:24 -0400

    basic input system

--- Assets/PlayerInputScript.cs
+++ Assets/PlayerInputScript.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInputScript : MonoBehaviour
 {
+
+    [SerializeField] public float playerSpeed = 5f;
+    [SerializeField] public float JumpHeight = 2f;
+    [SerializeField] public float gravity = -9.5f;
+
+    private CharacterController characterController;
+    private Vector3 moveInput;
+    private Vector3 velocity;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    // for flight idea
+    // turn off gravity and increase speed by X amount
+    // 
 }
