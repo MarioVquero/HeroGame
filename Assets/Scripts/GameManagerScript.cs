8de39e420259b02ddbb0df16449cddc88f652f6d
Lin33735 <161744729+Lin33735@users.noreply.github.com>
2025-09-09 14:11:17 -0400

    Added a Tigger to start the timer.

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [SerializeField] public GameObject timerStartPoint; //Trigger to start timer
+
+    [SerializeField] public float gameTimer;
+    public TMP_Text Timertext;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerStartPoint.GetComponent<TimerStarter>().timerStart) //Starts the timer when player triggers timerStartPoint
+        {
+            gameTimer += Time.deltaTime;
+
+            float seconds = Mathf.FloorToInt(gameTimer % 60);
+            Timertext.text = string.Format("Timer: {0:00}", seconds);
+
+        }
+    }
+}
