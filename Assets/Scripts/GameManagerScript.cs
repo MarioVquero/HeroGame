3326182bab018cf3830b0fcf3b01bea1b064d82e
Mario Vaquero <mariovquero@gmail.com>
2025-10-01 21:12:39 -0400

    city changes

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -5,38 +5,58 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    public TimerStarter timerStarter;
     [SerializeField] private GameObject timerStartPoint; //Trigger to start timer
     [SerializeField] private UIManager uIManager;
+    [SerializeField] private GameObject EndStatusOBJ;
 
-    public TimerStarter timerStarter;
+    public bool hasWon = false;
+
 
     public int score = 100;
 
-    [SerializeField] public float gameTimer;
+    [SerializeField] private float gameTimer;
+    [SerializeField] private float initialGameTime = 60f;
     public TMP_Text Timertext;
     public TMP_Text ScoreText;
+    public TMP_Text EndStatus;
+
     // Start is called before the first frame update
     void Start()
     {
-
-
+        EndStatusOBJ.SetActive(false);
+        // set initial gametime
+        gameTimer = initialGameTime;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (score >= 48)
+        {
+            hasWon = true;
+        }
 
         if (timerStarter.timerStart == true) //Starts the timer when player triggers timerStartPoint
         {
-            gameTimer += Time.deltaTime;
-
+            gameTimer -= Time.deltaTime;
+            Debug.Log(gameTimer);
             float minutes = Mathf.FloorToInt(gameTimer / 60);
             float seconds = Mathf.FloorToInt(gameTimer % 60);
 
-            if (seconds >= 30)
+            if (seconds <= 0 || score >=48 )
             {
+                EndStatusOBJ.SetActive(true);
+                if (hasWon == true && score >= 48)
+                {
+                    EndStatus.text = "YOU WIN";
+                }
+                else
+                {
+                    EndStatus.text = "YOU LOSE";
+                }
                 uIManager.Pause();
             }
 
